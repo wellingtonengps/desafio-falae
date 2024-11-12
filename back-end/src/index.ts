0ea761f06e85828816db65db0e27b6bbf9f440d3
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-11-12 13:46:10 -0300

    create router

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,16 +1,57 @@
 import express, {Express, Request, Response} from "express";
 import dotenv from "dotenv"
+import {PrismaClient} from "@prisma/client";
 
 dotenv.config();
 
+const prisma = new PrismaClient()
 const app: Express = express();
+
+
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (_,res: Response) => {
-    res.send('Olá, mundo')
+    res.send('server running...')
 });
+
+app.post('/api/auth/register',() => {
+
+})
+
+app.post('/api/products',() => {
+
+})
+
+app.get('/api/products',() => {
+
+})
+
+app.get('/api/products/:id',() => {
+
+})
+
+app.put('/api/products/:id',() => {
+
+})
+
+app.delete('/api/products/:id',() => {
+
+})
+
+app.post('/api/orders',() => {
+
+})
+
+app.post('/api/orders/:id', () => {
+
+})
+
+
+
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
 })
+
 
